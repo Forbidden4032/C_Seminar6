2927793cb813389428c51ced838150c6a3aa6de1
forbidden <forbidden@fedora.com>
2022-09-17 22:11:14 +0300

    remove comment

--- ex_43/Program.cs
+++ ex_43/Program.cs
@@ -6,7 +6,6 @@ void Points(double k1, double b1, double k2, double b2)
 {
    
         double x = (b2-b1)/(k1-k2); 
-        //double y = k1*((b2-b1)/(k1-k2)) + b1;
         double y = (k1*(b2-b1))/(k1-k2)+b1;
         if( k1 == k2) Console.WriteLine("Прямые не пересекаются");
         else Console.WriteLine($"Точка пересечения: ({x}; {y})");
